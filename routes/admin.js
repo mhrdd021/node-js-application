b7540ad3fb7d85764964bce908beaba4dfcfb637
mehrdadtorki <mehrdadtorki1381@gmail.com>
2024-05-02 18:24:53 +0330

    working with express

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path')
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+})
+
+router.post('/add-product', (req, res, next) => {
+    res.redirect('/');
+})
+
+module.exports = router;
